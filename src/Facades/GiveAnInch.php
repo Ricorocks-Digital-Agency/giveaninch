8208214b4402311a631ceb67a423ab6bdef75a6c
luke <lukeraymonddowning@gmail.com>
2020-11-10 16:58:59 +0000

    Adds facade doc.

--- src/Facades/GiveAnInch.php
+++ src/Facades/GiveAnInch.php
@@ -4,6 +4,14 @@ namespace RicorocksDigitalAgency\Giveaninch\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class GiveAnInch
+ * @package RicorocksDigitalAgency\Giveaninch\Facades
+ *
+ * @see \RicorocksDigitalAgency\Giveaninch\GiveAnInch
+ *
+ * @method static \RicorocksDigitalAgency\Giveaninch\GiveAnInch from($lat, $lng)
+ */
 class GiveAnInch extends Facade
 {
     protected static function getFacadeAccessor()
